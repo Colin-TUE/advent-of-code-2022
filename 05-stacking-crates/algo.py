bc0f7f5c63d810d2a534e5a8aa1491ba88845d30
Colin Lambrechts <colin.lambrechts@lego.com>
2022-12-08 17:44:50 +0100

    Add day 5 code.

--- 05-stacking-crates/algo.py
+++ 05-stacking-crates/algo.py
@@ -1,16 +1,66 @@
 import array
+import math
+import re
+
+
+def getTopContainers(stacks: list[list[str]]):
+    result = ""
+    for containers in stacks:
+        if len(containers) > 0:
+            result += containers[len(containers) - 1]
+
+    return result
 
 
 def compute(file):
     f = open(file, "r")
     lines = f.readlines()
 
-    line_array = array.array('u', [])
+    stacks: list[list[str]] = [[],  # Remains empty, since the stack numbers are 1 based
+                               [], [], [], [], [], [], [], [], []]
+    stacksManyMove: list[list[str]] = [[],  # Remains empty, since the stack numbers are 1 based
+                                       [], [], [], [], [], [], [], [], []]
 
     for line in lines:
-        line_array.append(line[0])
+        line = line.strip("\n")
+        matchContainers = re.match(r'( ){0,}(\[\w\])', line)
+        matchAction = re.fullmatch(r'move (\d+) from (\d) to (\d)', line)
+        matchStackNrs = re.fullmatch(r'(( \d )( )?){2,}', line)
+        # at least 2 times so the digit does not match the action line
 
-    result1 = 41
-    result2 = 256
+        if (matchContainers is not None):
+            # container line so add the containers to the right stack (1 based)
+            for index in range(1, len(line), 4):
+                container = line[index]
+                stackNr = math.floor(index / 4) + 1
+                if container != " ":
+                    stacks[stackNr].append(container)
+                    stacksManyMove[stackNr].append(container)
 
-    return result1, result2
+        elif (matchAction is not None):
+            # action line, so find all numbers and use them to move containers
+            groups = re.findall(r'move (\d+) from (\d) to (\d)', line)[0]
+
+            amount = int(groups[0])
+            source = int(groups[1])
+            target = int(groups[2])
+            for item in range(0, amount):
+                stacks[target].append(stacks[source].pop())
+
+            movedContainers = []
+            for item in range(0, amount):
+                movedContainers.append(stacksManyMove[source].pop())
+
+            for cont in movedContainers[::-1]:
+                stacksManyMove[target].append(cont)
+
+        elif (matchStackNrs is not None):
+            # Done reading the container stacks, so revert the insertion order
+            for index in range(0, len(stacks)):
+                stacks[index] = stacks[index][::-1]
+                stacksManyMove[index] = stacksManyMove[index][::-1]
+
+        else:
+            print(f"Line not matched: {line}")
+
+    return getTopContainers(stacks), getTopContainers(stacksManyMove)
--- 05-stacking-crates/test.py
+++ 05-stacking-crates/test.py
@@ -3,4 +3,4 @@ from algo import compute
 outputOne, outputMany = compute("example.csv")
 
 assert outputOne == 'CMZ', f"expected CMZ got {outputOne}"
-assert outputMany == 'CMZ', f"expected CMZ got {outputMany}"
+assert outputMany == 'MCD', f"expected MCD got {outputMany}"
